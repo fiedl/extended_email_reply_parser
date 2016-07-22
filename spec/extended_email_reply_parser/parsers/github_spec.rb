25b9f96a15f677bad436c1ebcde10a460e86cc7a
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2016-07-22 15:00:07 +0200

    improve spec

--- spec/extended_email_reply_parser/parsers/github_spec.rb
+++ spec/extended_email_reply_parser/parsers/github_spec.rb
@@ -8,13 +8,25 @@ describe ExtendedEmailReplyParser::Parsers::Github do
       before { @text = load_email 'email_1_2' }
 
       it { is_expected.to be_kind_of String }
-      it { is_expected.to include "Hi," }
-      it { is_expected.to include "On Tue, 2011-03-01 at 18:02 +0530, Abhishek Kona wrote:" }
-      it { is_expected.to include "What is the best way to clear a Riak bucket of all key" }
-      it { is_expected.to include "You can list the keys for the bucket and call delete for each." }
-      it { is_expected.to include "At the moment there is no straightforward way to delete" }
-      it { is_expected.not_to include "-Abhishek Kona" }
-      it { is_expected.not_to include "riak-users mailing list" }
+      it 'includes the new reply text itself' do
+        expect(subject).to include "Hi,"
+      end
+      it 'includes the new reply text, even after a quote' do
+        expect(subject).to include "You can list the keys for the bucket and call delete for each."
+      end
+      it 'includes the quote if it is followed by unquoted text' do
+        expect(subject).to include "> What is the best way to clear a Riak bucket of all key, values after"
+      end
+      it 'includes also the header line for the included quote' do
+        expect(subject).to include "On Tue, 2011-03-01 at 18:02 +0530, Abhishek Kona wrote:"
+      end
+      it 'does not include the quoted greeting, which is not followed by relevant text' do
+        expect(subject).not_to include "-Abhishek Kona"
+        expect(subject).not_to include "> -Abhishek Kona"
+      end
+      it 'does not include the mailing list footer' do
+        expect(subject).not_to include "riak-users mailing list"
+      end
     end
 
   end
